a9acecea643900aaf35e70ab5d90ab3b6e558ac9
Lavinia Bianca <chelaru_laviniabianca@yahoo.com>
2023-04-30 00:45:19 +0200

    Change grid on button click

--- script.js
+++ script.js
@@ -1,46 +1,54 @@
-
-
-function color(cell){
+function color(cell) {
     cell.classList.add("color");
 }
 
-function createGrid(size){
+function createGrid(size) {
     let tag,
-    text,
-    element, col;
+        text,
+        element, col;
 
-    for(let i = 0 ; i<size;i++){
-        col=document.createElement("div");
+    for (let i = 0; i < size; i++) {
+        col = document.createElement("div");
         col.classList.add("col");
-        element= document.getElementById("container");
+        element = document.getElementById("container");
         element.appendChild(col);
-        for(let j = 0 ; j<size;j++){
-            tag= document.createElement("div");
+        for (let j = 0; j < size; j++) {
+            tag = document.createElement("div");
             tag.classList.add("cell");
 
-            col=document.getElementsByClassName("col")[i];
+            col = document.getElementsByClassName("col")[i];
             col.appendChild(tag);
-            tag.setAttribute("onmouseover","color(event.currentTarget)");
-            
+            tag.setAttribute("onmouseover", "color(this)");
+
         }
-        
+
     }
 }
-// var got = { first : 0, second : 0 };
-//     function fire(event_type)
-//     {
-//       got[ event_type ] = 1;
-//       if( got.first && got.second )
-//       {
-//         //do stuff and reset
-//          got = { first : 0, second : 0 };
-//       }
-//     }
-// This is the listener part:
 
-//     document.onclick=function (){fire('first');}
-//     document.onmouseover=function (){fire('second');}
+function removeGrid() {
+    let container = document.getElementById("container");
+    let cols = container.children;
+    for (let i in cols) {
+        while (cols[i].firstChild) {
+            cols[i].removeChild(cols[i].lastChild);
+        }
+    }
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+
+}
+
+function changeGrid() {
+    let size = prompt("Enter a number less than 100");
+    if ( size<=100) {
+        removeGrid();
+        createGrid(size);
+    }
+    else{
+        changeGrid();
+    }
+}
 
 createGrid(16);
-
-// document.getElementsByClassName('cell').addEventListener("onmousedown" , color("event.composedPath[0]"),true);
+document.getElementById("btnSize").addEventListener("click", changeGrid);
